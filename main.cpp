8de92b0dfad1be87627b04e889148cc57c2a2b67
Colman-O <mojoshaughnessy@gmail.com>
2022-09-26 20:32:38 -0500

    Added race.h and programmed as necessary for the project. Too lazy to write the report for extra credit tho

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@ int main() {
     Queue<int> i;
     Queue<string> s;
 
+    // Test the Queues
     s.enqueue("I'm");
     s.enqueue("up");
     s.enqueue("next");
@@ -33,6 +34,32 @@ int main() {
     i.print();
     i.dequeue();
     i.print();
+
+    // Read formula race file and create vector
+    vector<Race> races;
+    getDataFromFile("../formula1_data_races.csv", races);
+
+    // Create Queue and Stack objects for formula 1 races
+    Queue<Race> f1q;
+    Stack<Race> f1s;
+
+    // Enqueue ten racers
+    for (int x = 0; x < 10; ++x) {
+        f1q.enqueue(races[x]);
+    }
+    f1q.print();
+
+    // Dequeue and push
+    Race moveOver;
+    for (int x = 0; x < 10; ++x) {
+        moveOver = f1q.dequeue();
+        f1s.push(moveOver);
+    }
+
+    // Pop and print
+    for (int x = 0; x < 10; ++x) {
+        cout << f1s.pop() << endl;
+    }
 
     return 0;
 }
